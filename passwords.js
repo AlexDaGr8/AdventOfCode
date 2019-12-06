ae692bcee211fda85a6eb14cdfbcadf22695e18b
Alexander Hinton <hintonalexander@gmail.com>
2019-12-06 09:43:29 -0500

    day4 part 2, and renaming files

--- passwords.js
+++ passwords.js
@@ -5,16 +5,30 @@ let combos = [...Array(inputRange[1] + 1).keys()];
 let lessCombos = combos.slice(inputRange[0])
 let asc = lessCombos.filter(ascFilter);
 let pairs = asc.filter(pairFilter);
-let pairsOfTwo = pairs.filter(pairOfTwoFilter);
-console.log(lessCombos)
-console.log(asc)
-console.log(pairs)
-console.log(pairsOfTwo)
+let pairsOfTwo = asc.filter(pairOfTwoFilter);
+console.log('pairsOfTwo', pairsOfTwo)
 
-console.log(pairOfTwoFilter("255599"))
-console.log(pairOfTwoFilter("266777"))
-console.log(pairOfTwoFilter("248888"))
-console.log(pairOfTwoFilter("333333"))
+let testCases = [
+    "234567",
+    "765432",
+    "234657",
+    "222222",
+    "223344",
+    "224433",
+    "255599",
+    "266777",
+    "266667",
+    "333333",
+    "111122",
+]
+
+testCases.forEach(test => {
+    console.log('--------------')
+    console.log('test', test);
+    console.log('asc', ascFilter(test));
+    console.log('pairs', pairFilter(test));
+    console.log('pairs of two', pairOfTwoFilter(test));
+})
 
 function ascFilter (d) {
     let digits = ("" + d).split("");
@@ -38,25 +52,33 @@ function pairFilter (d) {
 }
 function pairOfTwoFilter(d) {
     let digits = ("" + d).split("");
-    let pair = false;
+    //let pair = false;
+    let groupList = [];
     let group = 1;
+    let numPairs = 0;
     let toManyPairs = false;
     let last = +digits[0];
     digits.slice(1).forEach(d => {
         if (last === +d) { 
-            pair = true; 
+            //pair = true;
             group++;
+            numPairs += (group % 2 === 0 && group < 3) ? 1 : 0;
             if (group > 2) {
                 toManyPairs = true;
+                //numPairs -= (group % 2 !== 0) ? 1 : 0;
                 if (group % 2 === 0) {
                     toManyPairs = false;
+                    //numPairs++;
+                } else {
+                    numPairs -= 1;
                 }
             }
         } else {
+            groupList.push(group);
             group = 1;
         }
         last = +d;
-
     });
-    return pair && !toManyPairs;
+    groupList.push(group);
+    return groupList.some(d => d === 2);
 }
